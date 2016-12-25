961779b1776d2a3f1fca96599bce8378e0386320
KDiaCoding <KDiaCoding@users.noreply.github.com>
2016-12-25 17:33:41 +0100

    Update original-file.js

--- original-file.js
+++ original-file.js
@@ -1,12 +1,12 @@
--var Discord= require("discord.js")
--
--const client = new Discord.Client();
--
--bot.on("message", function(message)
--(
--  if(message.content=== "//pingy")
--  (
--  bot.reply(message, "pongy");
-- )
--  ));
--  bot.loginWithToken("MjU4MTMxMzY3MDM5MjA1Mzc2.CzEzgg.uT0A4qL3lqty0G7kS8XxrcwmqE8");
+var Discord= require("discord.js")
+
+const client = new Discord.Client();
+
+bot.on("message", function(message)
+(
+  if(message.content=== "//pingy")
+  (
+  bot.reply(message, "pongy");
+ )
+  ));
+bot.loginWithToken("MjU4MTMxMzY3MDM5MjA1Mzc2.CzEzgg.uT0A4qL3lqty0G7kS8XxrcwmqE8");
