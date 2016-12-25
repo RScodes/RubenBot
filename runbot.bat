50a864a65fdc0f961d24933c5ba0b6e0816ce70b
KDiaCoding <KDiaCoding@users.noreply.github.com>
2016-12-25 12:11:51 +0100

    Update runbot.bat

--- runbot.bat
+++ runbot.bat
@@ -1,11 +1,57 @@
 @echo off
-color 1
+color 0a
+title substitute bot
+:top
+echo ***************************************************************
+echo.
+echo options
+echo.
+echo ***************************************************************
+echo.
+echo Key: [1] start the bot
+echo [2] matrix
+echo [3] shutdown pc
+echo.
+echo [e] Exit
+echo.
+echo ***************************************************************
+echo Enter the number of the option you would like to select:
+echo.
+set /p udefine= 
+echo.
+echo ***************************************************************
+if %udefine%==1 goto ok
+if %udefine%==2 goto matrix
+if %udefine%==3 goto shutdown
+
+if %udefine%==e goto exit
+:exit
+exit
+:ok
 echo u wanna start the bot?
 pause
 u sure?
 pause
 echo alright then
 pause
+goto boot
 :boot
 node bot-first.js
 goto boot
+:matrix
+@echo off
+:start
+
+color 0a
+
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+
+goto start
+:shutdown
+start 
+start 
+start 
+start 
+start 
+start 
+start crash 
