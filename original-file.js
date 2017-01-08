29e200bff22a9de6e150aa1552a08dd6aed08cce
Kade Kenrick <KDiaCodes@users.noreply.github.com>
2017-01-08 19:28:45 +0100

    updated commands

--- original-file.js
+++ original-file.js
@@ -11,17 +11,21 @@ const prefix = "??";
 bot.on('message', message => {
 	if(message.author.bot) return;
 	if(!message.content.startsWith(prefix))return;
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
 	
-	if (message.content.startsWith(prefix + "ping")) {
+	let args = message.content.split(" ").slice(1);
+	
+	if (command === "ping")
 		message.channel.sendMessage("Hello my faggot dude");
-	}
+	} else 
 	
-	if (message.content.startsWith(prefix + "pussy")) {
+	if (command === "pussy") {
 		message.channel.sendMessage("You dirty minded Bastard http://pre13.deviantart.net/8539/th/pre/i/2011/327/7/5/pussy_cat_ii_by_antiplod-d4h254m.jpg")
-		
-	if (message.content.startsWith(prefix + "mother")) {
+	} else 	
+	if (command === "mother") {
 		message.channel.sendMessage("protect Mother Russia at all costs by killing Gerrmens");
-	}
+	} else 
 }
 });
 
