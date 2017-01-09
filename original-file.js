cfc7f847ac0acedaf331afc29d07c0fde2473e98
Kade Kenrick <KDiaCodes@users.noreply.github.com>
2017-01-09 13:28:27 +0100

    added crap

--- original-file.js
+++ original-file.js
@@ -11,29 +11,32 @@ const prefix = "??";
 bot.on('message', message => {
 	if(message.author.bot) return;
 	if(!message.content.startsWith(prefix))return;
+	
 	let command = message.content.split(" ")[0];
 	command = command.slice(prefix.length);
 	
 	let args = message.content.split(" ").slice(1);
 	
-	if (command === "ping")
+	if (command === "add") {
+		let numArray = args.map(n=> parseInt(n));
+		let total = numArray.reduce( (p, c) => p+c);
+		
+		message.channel.sendMessage(total);
+	}
+	if (command === "say"){
+		message.channel.sendMessage(args.join(" "));
+	}
+	if (command === "ping"){
 		message.channel.sendMessage("Hello my faggot dude");
-	} else 
+	}
 	
 	if (command === "pussy") {
-		message.channel.sendMessage("You dirty minded Bastard http://pre13.deviantart.net/8539/th/pre/i/2011/327/7/5/pussy_cat_ii_by_antiplod-d4h254m.jpg")
-	} else 	
+		message.channel.sendMessage("You dirty minded Bastard http://pre13.deviantart.net/8539/th/pre/i/2011/327/7/5/pussy_cat_ii_by_antiplod-d4h254m.jpg");
+	}	
+		
 	if (command === "mother") {
 		message.channel.sendMessage("protect Mother Russia at all costs by killing Gerrmens");
-	} else 
-	if (msg.content.startsWith(prefix + "perms")) {
-		let modRole = msg.guild.roles.find.("Owner", "Developer");
-    		if(msg.member.roles.has(modRole.id)) {
-   		 // Replys With Msg
-     		 msg.channel.sendMessage('Message');
-    	} else {
-     		 msg.reply("YOU FOOL! *You Dont Have Permission*")
-    }
+	}
 }	
 }
 });
