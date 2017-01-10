9a3a7ba7708204b5d86d521a689fe973b5e45b61
Kade Kenrick <KDiaCodes@users.noreply.github.com>
2017-01-10 13:35:27 +0100

    added eval command

--- original-file.js
+++ original-file.js
@@ -59,7 +59,20 @@ bot.on('message', message => {
 	if (command === "mother") {
 		message.channel.sendMessage("protect Mother Russia at all costs by killing Gerrmens");
 	}
-	
+	if(command === "eval")) {
+		if(!msg.author.id === "226003765889597440") return;
+		} else {
+    			try {
+      			var code = params.join(" ");
+     		 	var evaled = eval(code);
+
+      			if (typeof evaled !== "string")
+       		 	evaled = require("util").inspect(evaled);
+
+     		 	message.channel.sendCode("xl", clean(evaled));   
+   			 } catch(err) {
+     		 	message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+   	 }
 
 });
 
