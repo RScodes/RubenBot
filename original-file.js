332fcfd4329571d3c60d71f02377ee2361dec4f4
Kade Kenrick <KDiaCodes@users.noreply.github.com>
2017-01-10 18:23:03 +0100

    added .catch(console.error)

--- original-file.js
+++ original-file.js
@@ -11,11 +11,11 @@ bot.on('ready',() => {
 
 bot.on("guildMemberAdd", member =>{
 	let guild = member.guild;
-	guild.defaultChannel.sendMessage(`welcome ${member.user} to the land of coding`);
+	guild.defaultChannel.sendMessage(`welcome ${member.user} to the land of coding`).catch(console.error);
 });
 
 bot.on("guildCreate", guild => {
-	console.log(`New guild added : ${guild.name}, owned by ${guild.owner.user}`)      
+	console.log(`New guild added : ${guild.name}, owned by ${guild.owner.user}`).catch(console.error);      
 });
 
 bot.on("presenceUpdate", (oldMember, newMember) => {
@@ -24,9 +24,9 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
 	if(!playRole) return;
 	
 	if(newMember.user.presence.game && newMember.user.presence.game.name === "on the drums") {
-		newMember.addrole(playRole);
+		newMember.addrole(playRole).catch(console.error);
 	} else if(!newMember.user.presence.game && newMember.roles.has(playRole.id)) {
-		newMember.removeRole(playRole);
+		newMember.removeRole(playRole).catch(console.error);
 	}
 });
 
@@ -43,21 +43,21 @@ bot.on('message', message => {
 		let numArray = args.map(n=> parseInt(n));
 		let total = numArray.reduce( (p, c) => p+c);
 		
-		message.channel.sendMessage(total);
+		message.channel.sendMessage(total).catch(console.error);
 	}
 	if (command === "say"){
-		message.channel.sendMessage(args.join(" "));
+		message.channel.sendMessage(args.join(" ")).catch(console.error);
 	}
 	if (command === "ping"){
-		message.channel.sendMessage("Hello my faggot dude");
+		message.channel.sendMessage("Hello my faggot dude").catch(console.error);
 	}
 	
 	if (command === "pussy") {
-		message.channel.sendMessage("You dirty minded Bastard http://pre13.deviantart.net/8539/th/pre/i/2011/327/7/5/pussy_cat_ii_by_antiplod-d4h254m.jpg");
+		message.channel.sendMessage("You dirty minded Bastard http://pre13.deviantart.net/8539/th/pre/i/2011/327/7/5/pussy_cat_ii_by_antiplod-d4h254m.jpg").catch(console.error);
 	}	
 		
 	if (command === "mother") {
-		message.channel.sendMessage("protect Mother Russia at all costs by killing Gerrmens");
+		message.channel.sendMessage("protect Mother Russia at all costs by killing Gerrmens").catch(console.error);
 	}
 	if(command === "eval")) {
 		if(!msg.author.id === "226003765889597440") return;
@@ -71,7 +71,7 @@ bot.on('message', message => {
 
      		 	message.channel.sendCode("xl", clean(evaled));   
    			 } catch(err) {
-     		 	message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+     		 	message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``).catch(console.error);
    	 }
 	if (command === "gravityfalls"){
 		message.channel.sendMessage("https://tenor.co/xwJL.gif");
@@ -79,7 +79,7 @@ bot.on('message', message => {
 	if command === "purge")) {
 		let modRole = message.guild.roles.find("name", "ADMIN");
 		if(!message.member.roles.has(modRole.id)) {
-			return message.reply("pleb ur not admin")
+			return message.reply("pleb ur not admin").catch(console.error);
 		}
 		let messagecount = parseInt(params[0]);
 		message.channel.fetchMessages({limit: messagecount})
@@ -88,22 +88,23 @@ bot.on('message', message => {
 	if (command === "kick") {
 		let modRole = message.guild.roles.find("name", "ADMIN");
 		if(!message.member.roles.has(modRole.id)) {
-			return message.reply("pleb ur not admin")
+			return message.reply("pleb ur not admin").catch(console.error);
 		}
 		if(message.mention.user.size === 0) {
-			return message.reply("please mention a user to kick");
+			return message.reply("please mention a user to kick").catch(console.error);
 		}
 		let kickMember = message.guild.member(message.mentions.users.first());
 		if(!kickMember) {
-			return message.reply("that user dont exist bOi");
+			return message.reply("that user dont exist bOi").catch(console.error);
 		}
 		if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
-			return message.reply("i dont have the permission (KICK_MEMBER) to do this :sadface:")
+			return message.reply("i dont have the permission (KICK_MEMBER) to do this :sadface:").catch(console.error);
 		}
-		kickMember.kick();
+		kickMember.kick().then(member => {
+			message.reply(`${member.user.username} was kicked.`).catch(console.error);
+		}).catch(console.error)
+		}
 	}
-
-
 });
 
 
