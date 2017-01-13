74f3e440662a49722c4bf249a6b553ab0296c886
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-01-13 16:34:54 +0100

    added commands from the samuel branch

--- original-file.js
+++ original-file.js
@@ -7,6 +7,27 @@ bot.on('ready',() => {
 	console.log('I am ready!');
 });
 
+bot.on("guildMemberAdd", member =>{
+	let guild = member.guild;
+	guild.defaultChannel.sendMessage(`welcome ${member.user} to the land of coding`).catch(console.error);
+});
+
+bot.on("guildCreate", guild => {
+	console.log(`New guild added : ${guild.name}, owned by ${guild.owner.user}`).catch(console.error);      
+});
+
+bot.on("presenceUpdate", (oldMember, newMember) => {
+	let guild = newMember.guild;
+	let playRole = guild.roles.find("playing on the drums");
+	if(!playRole) return;
+	
+	if(newMember.user.presence.game && newMember.user.presence.game.name === "on the drums") {
+		newMember.addrole(playRole).catch(console.error);
+	} else if(!newMember.user.presence.game && newMember.roles.has(playRole.id)) {
+		newMember.removeRole(playRole).catch(console.error);
+	}
+});
+
 bot.on('message', message => {
 	if(message.author.bot) return;
 	if(!message.content.startsWith(config.prefix))return;
@@ -19,7 +40,15 @@ bot.on('message', message => {
 	if (command === "ping"){
 		message.channel.sendMessage("Hello my faggot dude").catch(console.error);
 	}
-	
+	if (command === "add") {
+		let numArray = args.map(n=> parseInt(n));
+		let total = numArray.reduce( (p, c) => p+c);
+		
+		message.channel.sendMessage(total).catch(console.error);
+	}
+	if (command === "say"){
+		message.channel.sendMessage(args.join(" ")).catch(console.error);
+	}
 	if (command === "pussy") {
 		message.channel.sendMessage("You dirty minded Bastard http://pre13.deviantart.net/8539/th/pre/i/2011/327/7/5/pussy_cat_ii_by_antiplod-d4h254m.jpg").catch(console.error);
 	}	
@@ -73,15 +102,24 @@ bot.on('message', message => {
 		// or the person who made the command: let member = msg.member;
 		// Add the role!
 		member.addRole(role).catch(console.error);
-		// Remove a role!
-		member.removeRole(role).catch(console.error);
 	}
 	if (command === "say"){
 		message.channel.sendMessage(args.join(" ")).catch(console.error);
 	}
+	if (command === "gravityfalls"){
+		message.channel.sendMessage("https://tenor.co/xwJL.gif");
+	} 
+	if (command === "purge") {
+		let modRole = message.guild.roles.find("name", "ADMIN");
+		if(!message.member.roles.has(modRole.id)) {
+			return message.reply("pleb ur not admin").catch(console.error);
+		}
+		let messagecount = parseInt(params[0]);
+		message.channel.fetchMessages({limit: messagecount})
+		.then(messages => message.channel.bulkDelete(messages));
+	}
 	if (command === "kick") {
 		let modRole = message.guild.roles.find("name", "ADMIN");
-		let userToKick = message.mentions.users.first();
 		if(!message.member.roles.has(modRole.id)) {
 			return message.reply("pleb ur not admin").catch(console.error);
 		}
@@ -95,10 +133,24 @@ bot.on('message', message => {
 		if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
 			return message.reply("i dont have the permission (KICK_MEMBER) to do this :sadface:").catch(console.error);
 		}
-		//we need to get a *GuildMember* object, mentions are only users. Then, we kick!
-		message.guild.member(userToKick).kick().then(member => {
+		kickMember.kick().then(member => {
 			message.reply(`${member.user.username} was kicked.`).catch(console.error);
-		});
+		}).catch(console.error)
+		}
+	}
+	if (command === "eval") {
+		if(message.author.id !== "226003765889597440") return;
+		try {
+			var code = args.join(" ");
+			var evaled = eval(code);
+
+			if (typeof evaled !== "string")
+				evaled = require("util").inspect(evaled);
+
+			message.channel.sendCode("xl", clean(evaled));   
+		} catch(err) {
+		 	message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+		}
 	}
 	if (command === "newrole") {
 		let modRole = message.guild.roles.find("name", "ADMIN");
@@ -114,20 +166,6 @@ bot.on('message', message => {
 		.then( newrole => {
 		newrole.edit({hoist: hoist, name: rolename, color: color})
 		}).catch(console.error);
-	}
-       if (command === "eval") {
-		if(message.author.id !== "226003765889597440") return;
-		try {
-			var code = args.join(" ");
-			var evaled = eval(code);
-
-			if (typeof evaled !== "string")
-				evaled = require("util").inspect(evaled);
-
-			message.channel.sendCode("xl", clean(evaled));   
-		} catch(err) {
-		 	message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-		}
 	}
 }); // END message handler
 
