54cdbb249d48ae6497a6c510e6b8d59a84defa3e
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-01-26 17:54:12 +0100

    added embed command

--- original-file.js
+++ original-file.js
@@ -52,6 +52,18 @@ bot.on('message', message => {
 		msg.edit("Yes im on and its been **=>** *" + Math.round(endTime - startTime) + "* ms since you had to say ping");
 		});
 	}
+	if (command === 'embed') {
+		let modRole = message.guild.roles.find("name", "ADMIN");
+		if(!message.member.roles.has(modRole.id)) {
+			return message.reply("pleb ur not admin").catch(console.error);
+		}
+		let args = message.content.split(" ").slice(1);
+		let text = args[0];
+		message.channel.sendEmbed({
+			color: 0xFFFFFF,
+			title: args.join(`${text}`),
+		});
+	}
 	if (command === "add") {
 		let numArray = args.map(n=> parseInt(n));
 		let total = numArray.reduce( (p, c) => p+c);
