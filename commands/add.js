174bc8c0f5dcb8b4002dcfcd272d0c47144f73ee
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-12 15:46:39 +0100

    Update and rename addrole.js to add.js

---
+++ commands/add.js
@@ -0,0 +1,6 @@
+exports.run = (bot, message, args) => {
+  let numArray = args.map(n=> parseInt(n));
+	let total = numArray.reduce( (p, c) => p+c);
+		
+	message.channel.sendMessage(total).catch(console.error);
+}
