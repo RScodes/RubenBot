6eb64e390bf6b129d62e9299318138ce948c79c3
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-12 15:53:00 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,3 +1,13 @@
 exports.run = (bot, message, args) => {
-
+  let modRole = message.guild.roles.find("name", "ADMIN");
+	let userToKick = message.mentions.users.first();
+	if(!message.member.roles.has(modRole.id)) {
+		return message.reply("pleb ur not admin").catch(console.error);
+	}
+	let role = message.guild.roles.find("name", "Team Mystic");
+	// Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
+	let member = message.guild.member(message.mentions.users.first());
+	// or the person who made the command: let member = msg.member;
+	// Add the role!
+	member.addRole(role).catch(console.error);
 }
