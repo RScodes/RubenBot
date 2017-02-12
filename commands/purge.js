03ee4e968750634ab3c8ea7abd1904933520da0d
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-12 15:48:40 +0100

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,7 +1,7 @@
 exports.run = (bot, message, args) => {
-  var amount = parseInt(args[1]);
-		msg.channel.fetchMessages({limit: amount})
-		.then(messages => {
-			messages.map(msg => msg.delete().catch(console.error) );
-		}).catch(console.error);
+	var amount = parseInt(args[1]);
+	message.channel.fetchMessages({limit: amount})
+	.then(messages => {
+		messages.map(msg => msg.delete().catch(console.error) );
+	}).catch(console.error);
 }
