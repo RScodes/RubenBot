bae7acf6a9f65b793292d4a6c89963e6b441d45d
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-12 15:59:18 +0100

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,21 +1,21 @@
 exports.run = (bot, message, args) => {
-    if(message.author.id !== "226003765889597440") return;
-		try {
-			var code = args.join(" ");
-			var evaled = eval(code);
-
-			if (typeof evaled !== "string")
-				evaled = require("util").inspect(evaled);
-
-			message.channel.sendCode("xl", clean(evaled));   
-		} catch(err) {
-		 	message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-		}
+	if(message.author.id !== "226003765889597440") return;
+	try {
+		var code = args.join(" ");
+		var evaled = eval(code);
+		if (typeof evaled !== "string") {
+			evaled = require("util").inspect(evaled);
+		}		
+		message.channel.sendCode("xl", clean(evaled));   
+	} catch(err) {
+	 	message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+	}
 }
 
 function clean(text) {
-	if (typeof(text) === "string")
+	if (typeof(text) === "string") {
 		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-	else
+	} else{
 		return text;
+	}
 }
