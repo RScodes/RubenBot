c745bc0197aac5ff80d2da1aa57a12e85e82ca18
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-12 16:09:15 +0100

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,12 +1,12 @@
 exports.run = (bot, message, args) => {
-    let modRole = message.guild.roles.find("name", "ADMIN");
-		if(!message.member.roles.has(modRole.id)) {
-			return message.reply("pleb ur not admin").catch(console.error);
-		}
-		let args = message.content.split(" ").slice(1);
-		let text = args[0];
-		message.channel.sendEmbed({
-			color: 0xFFFFFF,
-			title: args.join(`${text}`),
-		});
+	let modRole = message.guild.roles.find("name", "ADMIN");
+	if(!message.member.roles.has(modRole.id)) {
+		return message.reply("pleb ur not admin").catch(console.error);
+	}
+	let args = message.content.split(" ").slice(1);
+	let text = args[0];
+	message.channel.sendEmbed({
+		color: 0xFFFFFF,
+		title: args.join(`${text}`),
+	});
 }
