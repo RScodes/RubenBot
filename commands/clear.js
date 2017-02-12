c9e999bb8cb57f99da36a7d2c5b0a3c91f4e6d81
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-12 15:50:20 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,3 +1,10 @@
 exports.run = (bot, message, args) => {
-
+  let delamount = parseInt(args[1]) ? parseInt(args[1]) : 1;
+			msg.channel.fetchMessages({limit: 100})
+			.then(messages => {
+				msgar = messages.array();
+				msgar = msgar.filter(msg => msg.author.id === bot.user.id);
+				msgar.length = delamount + 1;
+				msgar.map(msg => msg.delete().catch(console.error));
+		});
 }
