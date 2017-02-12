7161ff7b3ba64f65e97f0046c2e6057d5ebb01fc
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-12 15:46:56 +0100

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,5 +1,5 @@
 exports.run = (bot, message, args) => {
-  let numArray = args.map(n=> parseInt(n));
+	let numArray = args.map(n=> parseInt(n));
 	let total = numArray.reduce( (p, c) => p+c);
 		
 	message.channel.sendMessage(total).catch(console.error);
