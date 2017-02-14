e1f9bf4dc59f341da5fb13f7cf91e2d25139d62a
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-14 14:00:23 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,4 +1,4 @@
-exports.run = (bot, message, args) {
+exports.run = (bot, message, args) => {
   // get arguments for the command, as: !prefix +
   let args = msg.content.split(" ").slice(1);
   // change the configuration in memory
@@ -6,5 +6,4 @@ exports.run = (bot, message, args) {
 
   // Now we have to save the file.
   fs.writeFile('./config.json', JSON.stringify(config), (err) => {if(err) console.error(err)});
-}
 }
