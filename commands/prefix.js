68a24851730ac93d61b5ceca1231bece2a416cce
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-14 14:06:00 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -5,9 +5,6 @@ exports.run = (bot, message, args) => {
 		return message.reply("pleb ur not admin").catch(console.error);
 	}
   let args = message.content.split(" ").slice(1);
-  // change the configuration in memory
   config.prefix = args[0];
-
-  // Now we have to save the file.
   fs.writeFile('./config.json', JSON.stringify(config), (err) => {if(err) console.error(err)});
 }
